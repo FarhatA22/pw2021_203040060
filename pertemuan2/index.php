90685ccb28467cb86a47970bc09863a253632c43
FarhatA22 <farhatabd21@gmail.com>
2021-02-18 01:50:01 +0700

    Menyelesaikan Video Pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,69 @@
+<?php
+/*
+Farhat Abdurachman Aldjaidi
+203040060
+https://github.com/FarhatA22/pw2021_203040060.git
+Pertemuan 2 - 17 Februari 2021
+Mempelajari Sintaks Dasar PHP
+*/
+?>
+
+<?php 
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
+// Standar Output
+// echo, print
+// print_r
+// var_dump
+
+// Penulisan Sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka
+// $nama = "Farhat Abdurachman Aldjaidi";
+// echo "Halo, Nama saya $nama";
+
+// Operator
+// aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// Penggabung String / concatenation / concat
+// .
+// $nama_depan = "Farhat";
+// $nama_tengah = "Abdurachman";
+// $nama_belakang = "Aldjaidi";
+// echo $nama_depan . " " . $nama_tengah . " " . $nama_belakang;
+
+// Assignment
+// =, +=, -=, *=, /=, %=, .=
+// $x = "6";
+// $x += "7";
+// echo $x
+// $nama = "Farhat";
+// $nama .= " ";
+// $nama .= "Abdurachman";
+// $nama .= " ";
+// $nama .= "Aldjaidi";
+// echo $nama
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump(1 == "1");
+
+// Identitas
+// ===, !==
+// var_dump(1 === "1");
+
+// Logika
+// &&, ||, ! 
+
+// $x = 30;
+// var_dump($x < 20 || $x % 2 == 0)
+?>
